5dce86038e0bf1af7c3a20f68a7ab016ac30f722
mb2o <mboomaars@gmail.com>
2019-05-04 16:20:21 +0200

    Updates

---
+++ precache-manifest.1d7acb8ba448d35c325b7f38633721f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ee85b1a34cebdce05a3277eca9dd9e",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "c63cdc962634b33f6b3e",
+    "url": "/contactmanager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d0b730daa31726ebbd7c",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "c63cdc962634b33f6b3e",
+    "url": "/contactmanager/static/js/2.8efe83c3.chunk.js"
+  },
+  {
+    "revision": "d0b730daa31726ebbd7c",
+    "url": "/contactmanager/static/js/main.5660fc9c.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
